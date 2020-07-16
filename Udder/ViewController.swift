ffc93ae02f4ee635b5ecfd1427b04e6cf1e242a0
Donald Gemig <dongemig@yahoo.com>
2020-07-16 18:02:50 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Donald Gemig on 7/16/20.
+//  Copyright © 2020 Garage Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
